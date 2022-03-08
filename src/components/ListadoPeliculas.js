f753ad2bff955ad9592478bd06b0aad840641608
Laura Oviedo <lauraoviedosocias@gmail.com>
2022-03-08 15:41:25 -0300

    agregue estilado

---
+++ src/components/ListadoPeliculas.js
@@ -0,0 +1,34 @@
+import { ListGroup } from "react-bootstrap"
+import { useState, useEffect } from "react"
+import TarjetaPeliculaHorizontal from "./TarjetaPeliculaHorizontal"
+import "../styles/ListadoPeliculas.scss"
+
+
+const ListadoPeliculas = ({categoria, tituloCategoria}) => {
+
+    const [peliculas, setPeliculas] = useState([])
+
+    useEffect(() => {
+        fetch(
+          `https://api.themoviedb.org/3/movie/${categoria}?api_key=92b7c9e2808de339886a0b75ca3aa28e&language=es-AR&page=1`
+        )
+          .then((res) => res.json())
+          .then((data) => setPeliculas(data.results));
+    }, [])
+
+
+    return (
+      <div className="contenedor-lista">
+        <div className="encabezado"><h3 className="titulo-categoria">{tituloCategoria}</h3></div>
+        {peliculas.map((elemento) => (
+          <TarjetaPeliculaHorizontal
+            imagen={`https://image.tmdb.org/t/p/w200/${elemento.poster_path}`}
+            tituloPelicula={elemento.title}
+
+          />
+        ))}
+      </div>
+    );
+}
+
+export default ListadoPeliculas;
