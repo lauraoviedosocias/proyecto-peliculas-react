c15cb44127e47a1fca9da3040008531d621c7bea
Laura Oviedo <lauraoviedosocias@gmail.com>
2022-03-10 09:38:01 -0300

    arreglo busqueda

--- src/components/Busqueda.js
+++ src/components/Busqueda.js
@@ -10,10 +10,11 @@ import { Col, Row } from "react-bootstrap";
 const Busqueda = () => {
     
     const [busqueda, setBusqueda] = useState([])
-    const [valorBusqueda, setValorBusqueda] = useState("undefined")
     const [searchParams, setSearchParams] = useSearchParams({
-        query: ""
+        query: "marvel"
     })
+    const [valorBusqueda, setValorBusqueda] = useState("")
+    
 
     const handleChange = (e) => {
         setSearchParams({
@@ -21,10 +22,10 @@ const Busqueda = () => {
         })
     }
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     setValorBusqueda(searchParams.query)
-    // }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setValorBusqueda(searchParams.query)
+    }
 
     const handleClick = () => {
         setValorBusqueda(searchParams.get("query"))
@@ -33,7 +34,7 @@ const Busqueda = () => {
     }
 
     useEffect(()=>{
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=92b7c9e2808de339886a0b75ca3aa28e&language=es-AR&query=${valorBusqueda}&page=1`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=92b7c9e2808de339886a0b75ca3aa28e&language=es-AR&query=${searchParams.get("query")}&page=1`)
             .then((res) => res.json())
             .then((data) => {
                 setBusqueda(data.results)
@@ -45,11 +46,11 @@ const Busqueda = () => {
     return (
         <div>
             <h2 className="p-3">Acá podés buscar tu peli favorita</h2>
-            <form className="form-group w-25">
+            <form className="form-group w-25" onSubmit={handleSubmit}>
             <div className="input-group mb-3 p-3">
                 <input type="text" className="form-control" placeholder="Nombre de la película" aria-label="Nombre de la película" aria-describedby="button-addon2" 
                 onChange={handleChange}
-                value={searchParams.query} 
+                 
                 />
                 <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handleClick}>Buscar</button>
             </div>
