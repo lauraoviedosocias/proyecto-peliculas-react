18d412937c54c39464bbb46cdb13fc3280dbaddc
Laura Oviedo <lauraoviedosocias@gmail.com>
2022-03-19 00:07:13 -0300

    agrego complemento de error

--- src/components/ErrorNoEncontrado.js
+++ src/components/ErrorNoEncontrado.js
@@ -1,9 +1,13 @@
+import "../styles/ErrorNoEncontrado.scss";
+
 const ErrorNoEncontrado = () => {
-    return (
-        <div>
-            <h1>404</h1>
-        </div>
-    )
-}
+  return (
+    <div className="contenedor-error">
+      <div className="error">
+        <h3 className="aviso-error">I have no memory of this page</h3>
+      </div>
+    </div>
+  );
+};
 
 export default ErrorNoEncontrado;
